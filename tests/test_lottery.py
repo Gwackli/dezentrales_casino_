93b7796f438e5319d1d89c8c3f5143d74153e946
Gian-Luca Held <gian-luca.held@bluewin.ch>
2022-09-15 18:20:03 +0200

    test for requiremnt for bet

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -1,12 +1,16 @@
-from brownie import Casino, accounts
+from brownie import Casino, accounts, reverts
 
+bet_num_min = 1
+bet_num_max = 10
 bet_num = 5
-bet_value = 1_000_000_000_000_000_000
+bet_value_min = 1_000_000_000_000_000_000
+bet_value = bet_value_min
 
 
 def test_place_bet():
     account = accounts[0]
     casino = Casino.deploy({"from": account})
+
     place_bet_transaction = casino.place_bet(
         bet_num, {"from": account, "value": bet_value}
     )
@@ -17,3 +21,21 @@ def test_place_bet():
     assert retrieved_bet_number == bet_num
     assert retrieved_bet_value == bet_value
     assert retrieved_bet_block == bet_block
+
+
+def test_min_value():
+    account = accounts[0]
+    casino = Casino.deploy({"from": account})
+    with reverts():
+        casino.place_bet(bet_num, {"from": account, "value": bet_value_min - 1})
+
+
+def test_range():
+    account = accounts[0]
+    casino = Casino.deploy({"from": account})
+    with reverts():
+        casino.place_bet(bet_num_min - 1, {"from": account, "value": bet_value})
+    casino.place_bet(bet_num_min, {"from": account, "value": bet_value})
+    with reverts():
+        casino.place_bet(bet_num_max + 1, {"from": account, "value": bet_value})
+    casino.place_bet(bet_num_max, {"from": account, "value": bet_value})
