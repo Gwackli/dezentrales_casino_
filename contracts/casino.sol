15c82f92989a7df75907f14bf6c1914d1ec9578e
Gian-Luca Held <gian-luca.held@bluewin.ch>
2022-09-15 21:26:26 +0200

    Kommentare place bet

--- contracts/casino.sol
+++ contracts/casino.sol
@@ -1,20 +1,28 @@
 pragma solidity ^0.6.6;
 
 contract Casino {
+    //jeweils gesetzte zahl, gewetteter Betrag und den Block speichern in den Variablen
     mapping(address => uint256) public block_numbers;
     mapping(address => uint256) public bet_numbers;
     mapping(address => uint256) public bet_values;
 
+    //definierte Werte
     uint256 min_value = 1_000_000_000_000_000_000;
     uint256 min_number = 1;
     uint256 max_number = 10;
 
+    //Funktion um eine Wette zu erstellen
     function place_bet(uint256 bet_number) public payable {
+        //uberprufen ob der mindestbetrag bezahlt wurde
         require(msg.value >= min_value, "Not enough Matic");
+
+        //uberprufen ob die gesetzte zahl im beriech der zufallszahl ist
         require(
             bet_number >= min_number && bet_number <= max_number,
             "Number not in range"
         );
+
+        //Die wichtigen Werte speichern
         block_numbers[msg.sender] = block.number;
         bet_numbers[msg.sender] = bet_number;
         bet_values[msg.sender] = msg.value;
@@ -29,6 +37,7 @@ contract Casino {
             uint256
         )
     {
+        //generieren der Zufallszahl
         return (
             bet_numbers[msg.sender],
             bet_values[msg.sender],
