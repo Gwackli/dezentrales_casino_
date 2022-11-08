b77c82305bbd679699a3cfbff5d0b114f23e02be
Gian-Luca Held <gian-luca.held@bluewin.ch>
2022-11-08 23:38:39 +0100

    only deploy once

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -1,4 +1,5 @@
 from brownie import Casino, accounts, reverts
+import pytest
 
 # deffinieren der bereiche und des mindestbetrag
 bet_num_min = 1
@@ -8,10 +9,18 @@ bet_value_min = 1_000_000_000_000_000_000
 bet_value = bet_value_min
 
 
-# testen ob eine einfache Wette abgeschlossen werden kann
-def test_place_bet():
-    account = accounts[1]
+@pytest.fixture(scope="module")
+def casino():
+    account = accounts[0]
+    # casino = Casino.deploy({"from": account})
     casino = Casino.deploy({"from": account})
+    casino.fill_bank({"from": accounts[3], "value": 10 * bet_value})
+    return casino
+
+
+# testen ob eine einfache Wette abgeschlossen werden kann
+def test_place_bet(casino):
+    account = accounts[1]
 
     place_bet_transaction = casino.place_bet(
         bet_num, range, {"from": account, "value": bet_value}
@@ -19,9 +28,7 @@ def test_place_bet():
     place_bet_transaction.wait(1)
     bet_block = place_bet_transaction.block_number
 
-    casino.fill_bank({"from": accounts[3], "value": 8 * bet_value})
-
-    casino.claim()
+    casino.claim({"from": account, "value": 0})
     """(
         retrieved_bet_number,
         retrieved_bet_value,
@@ -35,49 +42,47 @@ def test_place_bet():
 
 
 # testen ob der Ersteller des Contracts X Amount vom contract wieder entfernen kann
-def test_owner_can_empty_bank():
+def test_owner_can_empty_bank(casino):
     account = accounts[0]
-    casino = Casino.deploy({"from": account})
-    casino.fill_bank({"from": accounts[3], "value": 8 * bet_value})
+    casino.fill_bank({"from": accounts[3], "value": 2 * bet_value})
     casino.empty_bank(bet_value, {"from": account})
 
 
 # testen dass keine andere Person Geld entfernen kann
-def test_not_owner_cannot_empty_bank():
-    account = accounts[0]
-    casino = Casino.deploy({"from": account})
-    casino.fill_bank({"from": accounts[3], "value": 8 * bet_value})
+def test_not_owner_cannot_empty_bank(casino):
+    account = accounts[1]
+    casino.fill_bank({"from": accounts[3], "value": 2 * bet_value})
     with reverts():
-        casino.empty_bank(bet_value, {"from": accounts[1]})
+        casino.empty_bank(bet_value, {"from": account})
 
 
 # testen dass man nicht mit zu wenig beitreten kann
-def test_min_value():
+def test_min_value(casino):
     account = accounts[0]
-    casino = Casino.deploy({"from": account})
+    # casino = Casino.deploy({"from": account})
     with reverts():
         casino.place_bet(bet_num, range, {"from": account, "value": bet_value_min - 1})
 
 
 # teste, dass man nur auf erlaubte Zahlen setzten kann
-def test_range_min():
+def test_range_to_small(casino):
     account = accounts[0]
-    casino = Casino.deploy({"from": account})
+    # casino = Casino.deploy({"from": account})
     with reverts():
         casino.place_bet(bet_num_min - 1, range, {"from": account, "value": bet_value})
 
 
 # testen dass die mindestzahl geht
-def test_range_to_small():
+def test_range_min(casino):
     account = accounts[0]
-    casino = Casino.deploy({"from": account})
+    # casino = Casino.deploy({"from": account})
     casino.place_bet(bet_num_min, range, {"from": account, "value": bet_value})
 
 
 # testen dass über der range nicht geht
-def test_range_max():
+def test_range_to_big(casino):
     account = accounts[0]
-    casino = Casino.deploy({"from": account})
+    # casino = Casino.deploy({"from": account})
     with reverts():
         casino.place_bet(
             bet_num_min + range + 1, range, {"from": account, "value": bet_value}
@@ -85,7 +90,7 @@ def test_range_max():
 
 
 # testen dass das obere Ende noch geht
-def test_range_to_big():
+def test_range_max(casino):
     account = accounts[0]
-    casino = Casino.deploy({"from": account})
+    # casino = Casino.deploy({"from": account})
     casino.place_bet(bet_num_min + range, range, {"from": account, "value": bet_value})
